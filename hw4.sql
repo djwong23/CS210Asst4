e2a5188b4050b0fa83a9e54724cd0641a47aafab
Derek Wong <djwong23@gmail.com>
2022-04-27 02:58:50 -0400

    finished part 1

--- hw4.sql
+++ hw4.sql
@@ -1,46 +1,69 @@
-create table artist (
-    id smallint auto_increment primary key,
+create table artist(
+    id int auto_increment primary key,
     name varchar(50) not null unique
 );
 create table album(
-    albumID smallint auto_increment primary key,
-    albumName varchar(50), 
-    artistID smallint references artist,
+    albumID int auto_increment primary key,
+    albumName varchar(50) not null, 
+    artistID int references artist not null,
+    released date references song not null,
     unique(albumName, artistID)
 );
 create table genreList(
-    genreID smallint primary key,
+    genreID tinyint auto_increment primary key,
     name varchar(50) not null unique
 );
 create table songGenres(
-    songID smallint not null,
-    genreID smallint references genreList not null,
+    songID int not null,
+    genreID tinyint references genreList not null,
     primary key(songID, genreID)
 );
 create table song(
-    songID smallint primary key,
+    songID int auto_increment primary key,
     title varchar(50) not null, 
-    artistID smallint references artist not null,
-    albumID smallint references album,
+    artistID int references artist not null,
+    albumID int references album,
     releaseDate date not null,
     unique(title, artistID)
 );
 create table user(
-    userID smallint primary key,
-    username varchar(50) unique
+    userID int auto_increment primary key,
+    username varchar(50) not null unique 
 );
 create table playlistSongs(
-    playlistID references playlist,
-    songID references song,
+    playlistID int references playlist,
+    songID int references song,
     primary key(playlistID, songID)
-)
+);
 create table playlist(
-    playlistID smallint primary key,
-    name varchar(50) not null,
-    userID smallint not null,
-    unique(name, userID)
-)
+    playlistID int auto_increment primary key,
+    title varchar(50) not null,
+    created datetime not null,
+    userID int references user not null,
+    unique(title, userID)
+);
 
+create table AlbumRating(
+    albumID int references album not null,
+    userID int references user not null,
+    rating tinyInt check (rating >= 1 and rating <= 5),
+    created date,
+    primary key(albumID, userID, created)
+);
+create table SongRating(
+    songID int references song not null,
+    userID int references user not null,
+    rating tinyInt check (rating >= 1 and rating <= 5),
+    created date,
+    primary key(albumID, userID, created)
+);
+create table PlaylistRating(
+    playlistID int references playlist not null,
+    userID int references user not null,
+    rating tinyInt check (rating >= 1 and rating <= 5),
+    created date,
+    primary key(albumID, userID, created)
+);
 
  
 
