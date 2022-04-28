883198a36de697c021f5ceaea2727ba1244eb4a9
Derek Wong <djwong23@gmail.com>
2022-04-28 18:10:03 -0400

    formatting

--- hw4.sql
+++ hw4.sql
@@ -2,39 +2,46 @@ create table artist(
     id int auto_increment primary key,
     name varchar(50) not null unique
 );
+
 create table album(
     albumID int auto_increment primary key,
-    albumName varchar(50) not null, 
+    albumName varchar(50) not null,
     artistID int references artist not null,
     released date references song not null,
     unique(albumName, artistID)
 );
+
 create table genreList(
     genreID tinyint auto_increment primary key,
     name varchar(50) not null unique
 );
+
 create table songGenres(
     songID int not null,
     genreID tinyint references genreList not null,
     primary key(songID, genreID)
 );
+
 create table song(
     songID int auto_increment primary key,
-    title varchar(50) not null, 
+    title varchar(50) not null,
     artistID int references artist not null,
     albumID int references album,
     releaseDate date not null,
     unique(title, artistID)
 );
+
 create table user(
     userID int auto_increment primary key,
-    username varchar(50) not null unique 
+    username varchar(50) not null unique
 );
+
 create table playlistSongs(
     playlistID int references playlist,
     songID int references song,
     primary key(playlistID, songID)
 );
+
 create table playlist(
     playlistID int auto_increment primary key,
     title varchar(50) not null,
@@ -46,26 +53,32 @@ create table playlist(
 create table AlbumRating(
     albumID int references album not null,
     userID int references user not null,
-    rating tinyInt check (rating >= 1 and rating <= 5),
+    rating tinyInt check (
+        rating >= 1
+        and rating <= 5
+    ),
     created date,
     primary key(albumID, userID, created)
 );
+
 create table SongRating(
     songID int references song not null,
     userID int references user not null,
-    rating tinyInt check (rating >= 1 and rating <= 5),
+    rating tinyInt check (
+        rating >= 1
+        and rating <= 5
+    ),
     created date,
     primary key(albumID, userID, created)
 );
+
 create table PlaylistRating(
     playlistID int references playlist not null,
     userID int references user not null,
-    rating tinyInt check (rating >= 1 and rating <= 5),
+    rating tinyInt check (
+        rating >= 1
+        and rating <= 5
+    ),
     created date,
     primary key(albumID, userID, created)
 );
-
- 
-
-
-
